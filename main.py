c883e82c224a57a37c8812e90853c444f123d2bf
tkw <kawint2@gmail.com>
2025-04-26 07:13:25 -0700

    parse symbol, dest, comp and jump

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from parser import Parser
+from parser import Parser, COMMAND_TYPE
 
 def main():
     print("Hack Assembler")
     p = Parser("Add.asm")
     while p.hasMoreCommand:
         p.advance()
+        if p.currentCommandType == COMMAND_TYPE.A_COMMAND.value:
+            print(p.symbol())
+        elif p.currentCommandType == COMMAND_TYPE.C_COMMAND.value:
+            print(p.dest())
+            print(p.comp())
+            print(p.jump())
+        elif p.commandType == COMMAND_TYPE.L_COMMAND.value:
+            print(p.symbol())
 
 if __name__ == "__main__":
     main()
