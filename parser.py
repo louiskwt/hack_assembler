077bc89242ca9184afe591205745f73883c5efbe
tkw <kawint2@gmail.com>
2025-04-26 04:52:23 -0700

    handle commandType checking

--- parser.py
+++ parser.py
@@ -1,8 +1,14 @@
 import os, re
+import enum
+from utils import balanced
+
+class COMMAND_TYPE(enum.Enum):
+    A_COMMAND = 'A_COMMAND'
+    C_COMMAND = 'C_COMMAND'
+    L_COMMAND = 'L_COMMAND'
 
 class Parser:
     current = None
-    hasMoreCommand = None
     commands = []
     currentCommand = None
 
@@ -25,12 +31,25 @@ class Parser:
         except IOError as e:
             print("An Error occurred when opening file", e)
     
-    def advance(self):
+    def hasMoreCommand(self) -> bool:
+        return self.current < len(self.commands) - 1
+
+    def advance(self) -> None:
         if self.hasMoreCommand:
             self.current += 1
             if self.current == len(self.commands) - 1:
                 self.hasMoreCommand = False
             self.currentCommand = self.commands[self.current] 
-            print(self.currentCommand)
+            print(self.currentCommand + " " + self.commandType().value)
         else:
-            return None 
+            return None
+
+    def commandType(self) -> COMMAND_TYPE:
+        if "@" in self.currentCommand:
+            return COMMAND_TYPE.A_COMMAND
+        elif "=" in self.currentCommand:
+            return COMMAND_TYPE.C_COMMAND
+        elif balanced(self.currentCommand):
+            return COMMAND_TYPE.L_COMMAND
+        else:
+            raise("Invalid Command")
