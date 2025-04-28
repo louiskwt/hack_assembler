b0df5593bd5b865852d8b22c5fc9d54fe03399fc
Louis Tsang <kawingt2@gmail.com>
2025-04-28 16:07:25 +0800

    fix main

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 from parser import Parser, COMMAND_TYPE
 from code_generator import CodeGenerator
+from symbol_table import SymbolTable
 
 def main():
-    print("Hack Assembler")
-    file_name = "Add.asm" 
+    print("-------------Hack Assembler-------------")
+    file_name = "PongL.asm" 
     p = Parser(file_name)
     code = CodeGenerator()
-    out = open(file_name.split(".")[0] + ".hack", "+w")
-
+    symbols = SymbolTable()
+    # first pass
     while p.hasMoreCommand:
         p.advance()
         if p.currentCommandType == COMMAND_TYPE.A_COMMAND.value:
-            address = p.symbol()
-            out.write("{0:016b}".format(int(address)))
+            symbol = p.symbol()
+            if  not symbols.contains(symbol):
+                address = int(symbol) if symbol.isnumeric() else None
+                symbols.addEntry(symbol, address)
+        if p.currentCommandType == COMMAND_TYPE.L_COMMAND.value:
+            symbol = p.symbol()
+            if not symbols.contains(symbol):
+                symbols.addEntry(symbol, p.current)
+    
+    p.restart()
+    out = open(file_name.split(".")[0] + ".hack", "+w")
+    print(symbols.table)
+
+    # Second pass
+    while p.hasMoreCommand:
+        p.advance()
+        if p.currentCommandType == COMMAND_TYPE.A_COMMAND.value:
+            symbol = p.symbol()
+            print(symbol)
+            out.write("{0:016b}".format(symbols.getAddress(symbol)))
         elif p.currentCommandType == COMMAND_TYPE.C_COMMAND.value:
             dest_code = p.dest()
             dest = code.dest(dest_code if dest_code else "null")  
@@ -21,8 +40,9 @@ def main():
             jmp = code.jump(jmp_code if jmp_code else "null")
             out.write("{0:03b}".format(7) + comp + dest + jmp)
         elif p.commandType == COMMAND_TYPE.L_COMMAND.value:
-            print(p.symbol())
-        
+            symbol = p.symbol() 
+            out.write("{0:016b}".format(symbols.getAddress(symbol)))
+
         if p.current + 1 <= len(p.commands) - 1:
             out.write("\n")
     out.close()
